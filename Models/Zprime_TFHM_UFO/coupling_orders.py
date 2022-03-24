e0ccf633f18661ba04a5fe50832b720e55a610c8
Philipp Gadow <philipp.gadow@mytum.de>
2022-03-24 23:59:14 +0100

    add Ben Allanachs TFHM pheno model

---
+++ Models/Zprime_TFHM_UFO/coupling_orders.py
@@ -0,0 +1,20 @@
+# This file was automatically created by FeynRules 2.3.29
+# Mathematica version: 11.0.0 for Linux x86 (64-bit) (July 28, 2016)
+# Date: Mon 10 Dec 2018 13:04:26
+
+
+from object_library import all_orders, CouplingOrder
+
+
+NP = CouplingOrder(name = 'NP',
+                   expansion_order = 99,
+                   hierarchy = 1)
+
+QCD = CouplingOrder(name = 'QCD',
+                    expansion_order = 99,
+                    hierarchy = 1)
+
+QED = CouplingOrder(name = 'QED',
+                    expansion_order = 99,
+                    hierarchy = 2)
+
